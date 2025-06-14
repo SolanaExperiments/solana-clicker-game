b9eeea0beb5485f922e75e1fca7a3db2cd9f6b6a
SolanaExperiments <alexpr89@gmail.com>
2025-06-14 13:07:40 -0400

    Initial commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,11 @@
+# Contributing to Solana Clicker
+
+:tada: First off, thanks for taking the time to contribute!
+
+## Submitting PRs
+
+To keep styling of code consistent, please run the following before you submit a PR:
+
+    cd web-ui
+    yarn style  // will run Prettier on various files
+    yarn lint   // will ensure the preview build will run
---
+++ web-ui/idl/new-idl-ts.ts
@@ -0,0 +1,110 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/clicker.json`.
+ */
+export type Clicker = {
+  "address": "GwY9aAMD8nxhZxuTtPBbsFfgiqsVGkRTeA5fRyDjNkdM",
+  "metadata": {
+    "name": "clicker",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "click",
+      "discriminator": [
+        11,
+        147,
+        179,
+        178,
+        145,
+        118,
+        45,
+        186
+      ],
+      "accounts": [
+        {
+          "name": "game",
+          "writable": true
+        },
+        {
+          "name": "player",
+          "signer": true
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "initialize",
+      "discriminator": [
+        175,
+        175,
+        109,
+        31,
+        13,
+        152,
+        155,
+        237
+      ],
+      "accounts": [
+        {
+          "name": "game",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "player",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "game",
+      "discriminator": [
+        27,
+        90,
+        166,
+        125,
+        74,
+        100,
+        121,
+        18
+      ]
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "invalidPlayer"
+    }
+  ],
+  "types": [
+    {
+      "name": "game",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "player",
+            "type": "pubkey"
+          },
+          {
+            "name": "clicks",
+            "type": "u32"
+          }
+        ]
+      }
+    }
+  ]
+};
